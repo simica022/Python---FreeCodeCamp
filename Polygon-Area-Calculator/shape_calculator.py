298428971d9267861dd003196a49b5f3dcb1566c
simica022 <61891567+simica022@users.noreply.github.com>
2023-03-05 19:17:36 +0100

    Update shape_calculator.py

--- Polygon-Area-Calculator/shape_calculator.py
+++ Polygon-Area-Calculator/shape_calculator.py
@@ -1,6 +1,57 @@
 class Rectangle:
 
+    def __str__ (self):
+        return "Rectangle(width=" + str(self.width) + \
+           ", height=" + str(self.height) + ")"
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+    
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        rect = ("*" * self.width + "\n") * self.height
+        return rect
+        
+    def get_amount_inside(self, shape):
+        max_width = self.width // shape.width
+        max_height = self.height // shape.height
+        return max_width * max_height
 
 
+class Square(Rectangle):
 
-class Square:
+  def __init__(self, length):
+    super().__init__(length, length)
+
+  def __str__(self):
+    return "Square(side=" + str(self.width) + ")"
+
+  def set_side(self, side):
+    self.width = side
+    self.height = side
+
+  def set_width(self, side):
+    self.width = side
+    self.height = side
+
+  def set_height(self, side):
+    self.width = side
+    self.height = side
+
