843fcc191485a1f6a3074d827b3225dc9e936783
simica022 <61891567+simica022@users.noreply.github.com>
2023-03-07 12:20:52 +0100

    Update mean_var_std.py

--- Mean-Variance-Standard-Deviation-Calculator/mean_var_std.py
+++ Mean-Variance-Standard-Deviation-Calculator/mean_var_std.py
@@ -2,7 +2,29 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError ("List must contain nine numbers.")
 
+    lst = np.array(list).reshape(3,3)
 
+    calculations = {'mean':[],
+                    'variance':[],
+                    'standard deviation':[],
+                    'max':[],
+                    'min':[],
+                    'sum':[]}
+    
+    np_calculations = {'np.mean':'mean', 
+                        'np.var':'variance',
+                        'np.std':'standard deviation', 
+                        'np.max':'max',
+                        'np.min':'min',
+                        'np.sum':'sum'}
+    
+    for key, value in np_calculations.items():
+        func = eval(key)
+        calculations[value].append(func(lst, axis=0).tolist()) 
+        calculations[value].append(func(lst, axis=1).tolist())
+        calculations[value].append(func(lst))
 
     return calculations
